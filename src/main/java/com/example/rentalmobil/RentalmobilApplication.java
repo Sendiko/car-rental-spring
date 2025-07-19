f1d0d149bdaf026bf501183e4599516e84ab0ad3
Sendiko <sarangtawon897@gmail.com>
2025-07-19 18:50:32 +0700

    Initial Commit

---
+++ src/main/java/com/example/rentalmobil/RentalmobilApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentalmobil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalmobilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalmobilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rentalmobil/RentalmobilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentalmobil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalmobilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
