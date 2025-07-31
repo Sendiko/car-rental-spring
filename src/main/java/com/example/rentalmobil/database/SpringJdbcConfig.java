47d4ccc7b38256afc1b0a5e97036b9cd05a7a1e9
Sendiko <sarangtawon897@gmail.com>
2025-07-31 10:17:30 +0700

    Menambahkan JDBC Config

---
+++ src/main/java/com/example/rentalmobil/database/SpringJdbcConfig.java
@@ -0,0 +1,23 @@
+package com.example.rentalmobil.database;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan("com.baeldung.jdbc")
+public class SpringJdbcConfig {
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/rental_mobil_java");
+        dataSource.setUsername("rental_mobil");
+        dataSource.setPassword("rental_mobil");
+
+        return dataSource;
+    }
+}
